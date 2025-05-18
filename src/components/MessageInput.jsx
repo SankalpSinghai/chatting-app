9852ce03d15074ae0ca89e8f6a813415a263e1f1
Sankalp Singhai <sankalpsinghai@Sankalps-MacBook-Air.local>
2025-05-18 12:00:24 +0530

    fix: Handle empty input and enable send on enter

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -3,6 +3,14 @@ import { Send } from "lucide-react";
 
 
 export default function MessageInput({ onChange, myMsg, onMsgSent }) {
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            if (myMsg?.trim()) {
+                onMsgSent?.(myMsg?.trim());
+            }
+        }
+    }
     return (
         <div className="flex items-center space-x-2">
             <input
@@ -11,10 +19,11 @@ export default function MessageInput({ onChange, myMsg, onMsgSent }) {
                 value={myMsg || ''}
                 className="flex-1 border rounded-full px-2 py-4 focus:outline-none focus:border-blue-500"
                 onChange={onChange}
+                onKeyDown={handleKeyDown}
             />
             <button
                 className="bg-blue-600 text-white rounded-full p-2 hover:bg-blue-700 focus:outline-none disabled:opacity-50"
-                onClick={onMsgSent}
+                onClick={()=> onMsgSent?.(myMsg?.trim())}
                 disabled={!myMsg}>
                 <Send size={20} />
             </button>
